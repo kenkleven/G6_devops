b914f3ba1fa66322f085de4508488c690d9a417c
ken kleven <kenkleven50@gmail.com>
2024-03-10 11:10:14 +0100

    ajout de la page detail et design

--- article/urls.py
+++ article/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='accueil'),
+    path('details/<str:pk>/', views.details, name='details'),
 ]
--- article/views.py
+++ article/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 def home(request):
   
     return render(request, 'article/index.html')
+
+def details(request, pk):
+    #commande = Commande.objects.get(id=pk)
+    #context = {'commande':commande}
+    return render(request, 'article/details.html')
